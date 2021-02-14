4d152cb2e8ef14ec3564fcd0a8cc3f21cf74af4f
sena <mskucukk@outlook.com>
2021-02-14 19:41:34 +0300

    tekraredendeger

---
+++ src/tekrareden.java
@@ -0,0 +1,15 @@
+public class tekrareden {
+    public static void main(String[] args) {
+        int sayi[] = {12, 28, 75, 44, 28, 49};
+
+        for (int i = 0; i < sayi.length; i++) {
+
+            for (int j = i+1; j < sayi.length; j++) {
+                if (sayi[i] == sayi[j]) {
+                    System.out.println(sayi[i] + " ");
+                }
+            }
+
+        }
+    }
+}
